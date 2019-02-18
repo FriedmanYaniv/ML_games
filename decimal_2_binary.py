87dc1dc46aa6a9f9b74cc40d7031b32cdac937f7
Yaniv <yanivfriedman88@gmail.com>
2019-02-19 00:53:09 +0200

    sd

--- decimal_2_binary.py
+++ decimal_2_binary.py
@@ -19,40 +19,38 @@ def convertToBinary(int):
 # X = np.array(range(255), dtype=int)
 # y = np.array(list(map(lambda x: convertToBinary(x), X)))
 
-length = 60
-X = np.array([i * 2 * np.pi / length for i in range(length)])
-y = np.sin(X)
-# X = np.array([i / 20 for i in range(20)])
-# y = X * 2
+length = 300
+# X = np.array([i * 2 * np.pi / length for i in range(length)])
+# y = np.sin(X)
+X = np.array([((length/2) - i) / (length/20) for i in range(length)])
+y = X ** 2
 
-X = (np.random.random((100000))-.5) * 2 * np.pi
-y = np.sin(X)
+# X = (np.random.random((100000))-.5) * 2 * np.pi
+# y = np.sin(X)
 
 plt.plot(X, y, '.')
 
 model = Sequential()
-model.add(Dense(output_dim=5, input_dim=1))
-model.add(Activation("relu"))
-model.add(Dense(output_dim=5, input_dim=1))
-model.add(Activation("relu"))
-model.add(Dense(output_dim=5, input_dim=1))
-model.add(Activation("relu"))
-model.add(Dense(output_dim=5, input_dim=1))
-model.add(Activation("relu"))
-model.add(Dense(output_dim=1))
+model.add(Dense(output_dim=50, input_dim=1))
+# model.add(Dense(5, activation='sigmoid'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(50, activation='relu'))
+# model.add(Dense(50, activation='linear'))
+model.add(Dense(1, activation='linear'))
 model.compile(loss='mean_squared_error',
-              optimizer='sgd',
+              optimizer='adam',
               metrics=['accuracy'])
 model.summary()
-model.fit(X, y, batch_size=32, nb_epoch=5, verbose=1)
+model.fit(X, y, batch_size=32, nb_epoch=500, verbose=1)
 
-X_test = X + 0.3
+X_test = np.interp(X, [np.min(X), np.max(X)], [-30, 30])
 y_pred = np.squeeze(model.predict(X_test))
 plt.plot(X_test, y_pred, '*')
-y_pred = np.squeeze(model.predict(X))
-plt.plot(X, y_pred, '*')
+# y_pred = np.squeeze(model.predict(X))
+# plt.plot(X, y_pred, '*')
 
-###aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
 ############################################################################
 ############################################################################
 ############################################################################
